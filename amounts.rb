21ff9addd2239f85f02bcf4a7ca14d2d75022877
Misa2523 <sa5chsgrnccon2510@gmail.com>
2024-09-13 05:15:48 +0000

    Gitで保存&管理

--- amounts.rb
+++ amounts.rb
@@ -3,6 +3,6 @@
 #データの先頭から順に繰り返して処理が行われる
 
 amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
-amounts.each do |fruit, amounts|   ##ハッシュの内容を順にキーをfruit、値をamountに代入して繰り返す
+amounts.each do |fruit, amounts|   #ハッシュの内容を順にキーをfruit、値をamountに代入して繰り返す
   puts "#{fruit}は#{amounts}個です。"
 end
